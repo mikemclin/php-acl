9956cf25a2a85118ad4711fbe94c170a7da49a89
Mike McLin <mclinmike@gmail.com>
2015-07-12 11:55:15 -0400

    Code format

--- src/Acl.php
+++ src/Acl.php
@@ -126,7 +126,10 @@ class Acl implements AclContract, StorableContract
     public function can($ability)
     {
         foreach ($this->roles as $role) {
-            if (is_array($this->abilities[$role]) && in_array($ability, $this->abilities[$role])) {
+            if (
+                is_array($this->abilities[$role]) &&
+                in_array($ability, $this->abilities[$role])
+            ) {
                 return true;
             }
         }
